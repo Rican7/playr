33f11a5df6264184cc109eb5132056a1c9176516
Trevor Suarez <rican7@gmail.com>
2013-02-23 23:13:22 -0500

    Fixing models. They're awesome.

--- app/lib/models/queue.js
+++ app/lib/models/queue.js
@@ -1,14 +1,15 @@
-var _ = require("underscore");
-
 module.exports = function () {
- 	this.songs = [];
+	// Private properties
 	var current = 0;
+
+	// Public properties
+ 	this.songs = [];
+
 	return {
 		addSong: function (song, callback) {
 			this.songs.push(song);
 			callback(this);
-		}
-
+		},
 		removeSongAtIndex: function(index, callback) {
 			this.songs.splice(index, 1);
 			callback(this);
