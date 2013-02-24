539a8d137f614aa42aa1ec47cb3ccb8ef124d050
Eli Perkins <eli@onemightyroar.com>
2013-02-24 03:25:06 -0500

    When a text comes in, send it out using sms.js and notify via sockets

--- app/server.js
+++ app/server.js
@@ -15,7 +15,7 @@ var twilioClient = require('twilio')('ACceb22beac0d0c3ca5337f63739a1fbe3', 'f6a7
 
 // Internal libs
 var music = require('./lib/music.js')(rdio, Models);
-
+var sms = require('./lib/sms.js')(twilioClient);
 
 // Create our app
 var app = express.createServer(express.logger());
@@ -86,6 +86,7 @@ app.post('/sms/reply/', function(request, response) {
 
 		// Did we get a response?
 		if ( data !== null ) {
+      io.sockets.emit('track-create', data);
 			// Set our success message
 			var responseMessage = "Thanks for searching for " + searchParam + "! You just might hear your song soon. ;)";
 		}
@@ -94,27 +95,10 @@ app.post('/sms/reply/', function(request, response) {
 			var responseMessage = "Sorry, we couldn't find a track matching your search: " + searchParam;
 		}
 
-		// Send a response
-		twilioClient.sms.messages.post(
-			{
-				to: sender,
-				from: "+16032623095",
-				body: responseMessage
-			},
-			// Twilio response callback
-			function(err, responseData) {
-				// "err" is an error received during the request, if any
-				if (!err) {
-					// "responseData" is a JavaScript object containing data received from Twilio.
-					// A sample response from sending an SMS message is here (click "JSON" to see how the data appears in JavaScript):
-					// http://www.twilio.com/docs/api/rest/sending-sms#example-1
+    // Send a response
+    sms.incomingSms(sender, responseMessage, function (error, data) {
 
-					console.log(responseData.from); // outputs "+14506667788"
-					console.log(responseData.body); // outputs "word to your mother."
-
-				}
-			}
-		);
+    });
 	});
 });
 
